92fe32ead10b46d47e4e74e998707bf761f93cc9
otavioxavier <otavioxaviermarinho@gmail.com>
2024-07-06 03:20:21 -0300

    :construction: adding the base structure

--- src/app/workout/page.tsx
+++ src/app/workout/page.tsx
@@ -1,8 +1,14 @@
+import WorkoutList from '@/components/workout/workout-list';
+
 export default async function Workout() {
 
   return (
     <main>
-
+      <header className='mb-16'>
+       <h1 className='text-2xl font-bold'>My <span className='text-red'>Workout</span></h1>
+       <h2 className='text-slate-500'>Today is (<span className='text-red-500'>chest and triceps</span>).</h2>
+      </header>
+      <WorkoutList exercises={null}/>
     </main>
   )
 }
