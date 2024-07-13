38e700fd3e6e537414624cfcf1f4f85242a4feb9
otavioxavier <otavioxaviermarinho@gmail.com>
2024-07-13 19:48:07 -0300

    :bug:fix: it wasn't building

--- src/app/api/exercise/[id]/route.ts
+++ src/app/api/exercise/[id]/route.ts
@@ -5,7 +5,13 @@ import { schema } from "@/lib/schema";
 
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
   try {
-    // { name, description, thumbUrl, planId, muscle }
+
+    const { userId } = auth();
+
+    if (!userId) {
+      return Response.json({ status: 401, message: "Unauthorized" });
+    }
+
     const id = params.id;
 
     if(!id) {
@@ -18,20 +24,14 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
       return Response.json({ status: 401, message: "Missing fields" });
     }
 
-    const { userId } = auth();
-
-    if (!userId) {
-      return Response.json({ status: 401, message: "Unauthorized" });
-    }
-
     const exercise = await prismadb.exercise.update({
       where: {id},
-      data:{...data},
+      data,
     });
 
     return Response.json({ status: 201, data: exercise });
   } catch (error: any) {
-    console.error("Error at /api/exercise POST", error);
+    console.error("Error at /api/exercise/:id PUT", error);
     return Response.json({ status: 500, message: "Internal Server Error" });
   }
 }
