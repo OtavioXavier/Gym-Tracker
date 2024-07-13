34e079469606aea36fac8f8771765fe2495564bb
otavioxavier <otavioxaviermarinho@gmail.com>
2024-07-13 20:33:31 -0300

    :recycle:refactor: update exercise route

--- src/app/api/exercise/[id]/route.ts
+++ src/app/api/exercise/[id]/route.ts
@@ -1,37 +1,37 @@
+
+Copy code
 import { auth } from "@clerk/nextjs/server";
 import prismadb from "@/lib/prismadb";
-
 import { schema } from "@/lib/schema";
 
 export async function PUT(req: Request, { params }: { params: { id: string } }) {
   try {
-
     const { userId } = auth();
 
     if (!userId) {
-      return Response.json({ status: 401, message: "Unauthorized" });
+      return new Response(JSON.stringify({ message: "Unauthorized" }), { status: 401 });
     }
 
     const id = params.id;
 
-    if(!id) {
-      return Response.json({ status: 401, message: "This exercise does not exist" });
+    if (!id) {
+      return new Response(JSON.stringify({ message: "This exercise does not exist" }), { status: 404 });
     }
 
     const data = schema.parse(await req.json());
 
-    if(!data) {
-      return Response.json({ status: 401, message: "Missing fields" });
+    if (!data) {
+      return new Response(JSON.stringify({ message: "Missing fields" }), { status: 400 });
     }
 
     const exercise = await prismadb.exercise.update({
-      where: {id},
+      where: { id },
       data,
     });
 
-    return Response.json({ status: 201, data: exercise });
+    return new Response(JSON.stringify({ data: exercise }), { status: 200 });
   } catch (error: any) {
     console.error("Error at /api/exercise/:id PUT", error);
-    return Response.json({ status: 500, message: "Internal Server Error" });
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), { status: 500 });
   }
 }
